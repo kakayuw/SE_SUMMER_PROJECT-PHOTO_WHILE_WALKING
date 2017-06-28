23cbde1e67f4144d43181cb3e4c4342acae79aa6
MegaPikachu <1025151692@qq.com>
2017-06-28 15:50:33 +0800

    no message

--- bzbp/src/main/java/service/AppService.java
+++ bzbp/src/main/java/service/AppService.java
@@ -2,10 +2,7 @@ package service;
 
 import java.util.List;
 
-import model.Book;
-import model.Bookdetail;
-import model.Order;
-import model.Orderitem;
+
 import model.User;
 
 /**
@@ -13,54 +10,6 @@ import model.User;
  * 
  */
 public interface AppService {
-
-	/**
-	 * book
-	 * 
-	 */
-	public Integer addBook(Book book);
-
-	public void addBookpicture(int id, String source);
-	
-	public void addBookdetail(Bookdetail bookdetail);
-
-	public void deleteBook(Book book);
-
-	public void updateBook(Book book);
-
-	public Book getBookById(int id);
-	
-	public Bookdetail getBookdetailById(int id);
-
-	public List<Book> getAllBooks();
-
-	/**
-	 * order
-	 * 
-	 */
-	public Integer addOrder(Order order);
-
-	public void deleteOrder(Order order);
-
-	public void updateOrder(Order order);
-
-	public Order getOrderById(int id);
-
-	public List<Order> getAllOrders();
-
-	/**
-	 * order item
-	 * 
-	 */
-	public Integer addOrderitem(Orderitem orderitem);
-
-	public void deleteOrderitem(Orderitem orderitem);
-
-	public void updateOrderitem(Orderitem orderitem);
-
-	public Orderitem getOrderitemById(int id);
-
-	public List<Orderitem> getAllOrderitems();
 
 	/**
 	 * user
--- bzbp/src/main/java/service/impl/AppServiceImpl.java
+++ bzbp/src/main/java/service/impl/AppServiceImpl.java
@@ -2,139 +2,26 @@ package service.impl;
 
 import java.util.List;
 
-import model.Book;
-import model.Bookdetail;
-import model.Order;
-import model.Orderitem;
+
 import model.User;
 import service.AppService;
-import dao.BookDao;
-import dao.BookdetailDao;
-import dao.LoginDao;
-import dao.OrderDao;
-import dao.OrderitemDao;
+
+
 import dao.UserDao;
-import dao.impl.LoginDaoImpl;
 
 /**
- * @author seniyuting
  * @version 1.0
  * 
  */
 public class AppServiceImpl implements AppService {
 
-	private BookDao bookDao;
-	private BookdetailDao bookdetailDao;
-	private OrderDao orderDao;
-	private OrderitemDao orderitemDao;
+
 	private UserDao userDao;
 
-	public void setBookDao(BookDao bookDao) {
-		this.bookDao = bookDao;
-	}
-	
-	public BookdetailDao getBookdetailDao() {
-		return bookdetailDao;
-	}
-	
-	public void setBookdetailDao(BookdetailDao bookdetailDao) {
-		this.bookdetailDao = bookdetailDao;
-	}
 
-	public void setOrderDao(OrderDao orderDao) {
-		this.orderDao = orderDao;
-	}
-
-	public void setOrderitemDao(OrderitemDao orderitemDao) {
-		this.orderitemDao = orderitemDao;
-	}
 
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
-	}
-
-	/**
-	 * book
-	 * 
-	 */
-	public Integer addBook(Book book) {
-		return bookDao.save(book);
-	}
-	
-	public void addBookdetail(Bookdetail bookdetail){
-		bookdetailDao.saveOrUpdate(bookdetail);
-	}
-	
-	public void addBookpicture(int id, String source){
-		bookdetailDao.savePicture(id, source);
-	}
-
-	public void deleteBook(Book book) {
-		bookDao.delete(book);
-	}
-
-	public void updateBook(Book book) {
-		bookDao.update(book);
-	}
-
-	public Book getBookById(int id) {
-		return bookDao.getBookById(id);
-	}
-	
-	public Bookdetail getBookdetailById(int id) {
-		return bookdetailDao.findById(id);
-	}
-
-	public List<Book> getAllBooks() {
-		return bookDao.getAllBooks();
-	}
-
-	/**
-	 * order
-	 * 
-	 */
-	public Integer addOrder(Order order) {
-		return orderDao.save(order);
-	}
-
-	public void deleteOrder(Order order) {
-		orderDao.delete(order);
-	}
-
-	public void updateOrder(Order order) {
-		orderDao.update(order);
-	}
-
-	public Order getOrderById(int id) {
-		return orderDao.getOrderById(id);
-	}
-
-	public List<Order> getAllOrders() {
-		return orderDao.getAllOrders();
-	}
-
-	/**
-	 * order item
-	 * 
-	 */
-	public Integer addOrderitem(Orderitem orderitem) {
-		return orderitemDao.save(orderitem);
-	}
-
-	public void deleteOrderitem(Orderitem orderitem) {
-		orderitemDao.delete(orderitem);
-	}
-
-	public void updateOrderitem(Orderitem orderitem) {
-		orderitemDao.update(orderitem);
-	}
-
-	public Orderitem getOrderitemById(int id) {
-		return orderitemDao.getOrderitemById(id);
-	}
-
-	public List<Orderitem> getAllOrderitems() {
-		return orderitemDao.getAllOrderitems();
 	}
 
 	/**
