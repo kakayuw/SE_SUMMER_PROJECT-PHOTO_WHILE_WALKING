87baa4b6eab6ec2a4c696578fa9223bc67310550
刘瑾旭 <1010878473@qq.com>
2017-06-29 09:25:54 +0800

    no message

--- iter1/app/src/main/java/com/sourcey/materiallogindemo/LoginActivity.java
+++ iter1/app/src/main/java/com/sourcey/materiallogindemo/LoginActivity.java
@@ -1,8 +1,5 @@
-package com.example.xianyutuci.iter1.Activity;
+package com.sourcey.materiallogindemo;
 
-/**
- * Created by liujinxu on 17/6/27.
- */
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -16,23 +13,39 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.ButterKnife;
-import butterknife.InjectView;
+import butterknife.Bind;
+
+import com.google.gson.Gson;
+
+import Model.User;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
 
-    @Bind(R.id.input_email) EditText _emailText;
+    public static String ip="192.168.1.184:8080/BZBP";
+    public boolean valid;
+
+    @Bind(R.id.input_username) EditText _usernameText;
     @Bind(R.id.input_password) EditText _passwordText;
     @Bind(R.id.btn_login) Button _loginButton;
     @Bind(R.id.link_signup) TextView _signupLink;
-
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ButterKnife.inject(this);
-
+        ButterKnife.bind(this);
+        
         _loginButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -48,8 +61,56 @@ public class LoginActivity extends AppCompatActivity {
                 // Start the Signup activity
                 Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
                 startActivityForResult(intent, REQUEST_SIGNUP);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });
+    }
+
+    public void sendHttpPost(String getUrl, User user) {
+        HttpURLConnection urlConnection = null;
+        URL url = null;
+        try {
+            url = new URL(getUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();//打开http连接
+            urlConnection.setConnectTimeout(3000);//连接的超时时间
+            urlConnection.setUseCaches(false);//不使用缓存
+            //urlConnection.setFollowRedirects(false);是static函数，作用于所有的URLConnection对象。
+            urlConnection.setInstanceFollowRedirects(true);//是成员函数，仅作用于当前函数,设置这个连接是否可以被重定向
+            urlConnection.setReadTimeout(3000);//响应的超时时间
+            urlConnection.setDoInput(true);//设置这个连接是否可以写入数据
+            urlConnection.setDoOutput(true);//设置这个连接是否可以输出数据
+            urlConnection.setRequestMethod("POST");//设置请求的方式
+            urlConnection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");//设置消息的类型
+            urlConnection.connect();// 连接，从上述至此的配置必须要在connect之前完成，实际上它只是建立了一个与服务器的TCP连接
+            Gson gson = new Gson();
+            String jsonstr = gson.toJson(user);
+
+            //------------字符流写入数据------------
+            OutputStream out = urlConnection.getOutputStream();//输出流，用来发送请求，http请求实际上直到这个函数里面才正式发送出去
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));//创建字符流对象并用高效缓冲流包装它，便获得最高的效率,发送的是字符串推荐用字符流，其它数据就用字节流
+            bw.write(jsonstr);//把json字符串写入缓冲区中
+            bw.flush();//刷新缓冲区，把数据发送出去，这步很重要
+            out.close();
+            bw.close();//使用完关闭
+
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {//得到服务端的返回码是否连接成功
+
+                //------------字符流读取服务端返回的数据------------
+                InputStream in = urlConnection.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                String str = null;
+                str = br.readLine();//BufferedReader特有功能，一次读取一行数据
+                in.close();
+                br.close();
+                valid = str.equals("success");
+            }
+
+        } catch (Exception e) {
+
+        } finally {
+            urlConnection.disconnect();//使用完关闭TCP连接，释放资源
+        }
     }
 
     public void login() {
@@ -68,20 +129,36 @@ public class LoginActivity extends AppCompatActivity {
         progressDialog.setMessage("Authenticating...");
         progressDialog.show();
 
-        String email = _emailText.getText().toString();
+        String username = _usernameText.getText().toString();
         String password = _passwordText.getText().toString();
+
+        final User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                sendHttpPost("http://"+ip+"/login",user);
+            }}).start();
+
 
         // TODO: Implement your own authentication logic here.
 
-        new android.os.Handler().postDelayed(
+
+
+       new android.os.Handler().postDelayed(
                 new Runnable() {
                     public void run() {
+                        if(valid){
                         // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
+                        onLoginSuccess();}
+                        else{
+                         onLoginFailed();}
                         progressDialog.dismiss();
                     }
-                }, 3000);
+                }, 1000);
+
     }
 
 
@@ -99,7 +176,7 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        // disable going back to the MainActivity
+        // Disable going back to the MainActivity
         moveTaskToBack(true);
     }
 
@@ -115,25 +192,25 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public boolean validate() {
-        boolean valid = true;
+        boolean valid_1 = true;
 
-        String email = _emailText.getText().toString();
+        String username = _usernameText.getText().toString();
         String password = _passwordText.getText().toString();
 
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
-            valid = false;
+        if (username.isEmpty()) {
+            _usernameText.setError("enter a valid username");
+            valid_1 = false;
         } else {
-            _emailText.setError(null);
+            _usernameText.setError(null);
         }
 
         if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
             _passwordText.setError("between 4 and 10 alphanumeric characters");
-            valid = false;
+            valid_1 = false;
         } else {
             _passwordText.setError(null);
         }
 
-        return valid;
+        return valid_1;
     }
 }
